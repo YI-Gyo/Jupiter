5d690a3ff9463293e49903b9f9925f5f218c3503
YIWEN GUO <33103135+YI-Gyo@users.noreply.github.com>
2019-04-19 11:14:21 -0700

    First commit

---
+++ src/internal/Item.java
@@ -0,0 +1,133 @@
+package internal;
+
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Item {
+	private String itemId;
+	private String name;
+	private double rating;
+	private String address;
+	private Set<String> categories;
+	private String imageUrl;
+	private String url;
+	private double distance;
+	
+	/**
+	 * This is a builder pattern in Java.
+	 */
+	private Item(ItemBuilder builder) {
+		this.itemId = builder.itemId;
+		this.name = builder.name;
+		this.rating = builder.rating;
+		this.address = builder.address;
+		this.categories = builder.categories;
+		this.imageUrl = builder.imageUrl;
+		this.url = builder.url;
+		this.distance = builder.distance;
+	}
+
+	// itembuilder is static 这样就不需要一个item实例便可有itembuilder。
+	
+	public static class ItemBuilder {
+		private String itemId;
+		private String name;
+		private double rating;
+		private String address; //null
+		private Set<String> categories;
+		private String imageUrl;
+		private String url;
+		private double distance;
+		
+		// 若把所有setter的返回值设为this则可以串成一串赋值
+		// like：builder.setItermId.setName
+		// 没有set的field取其默认值
+		
+		public void setItemId(String itemId) {
+			this.itemId = itemId;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public void setRating(double rating) {
+			this.rating = rating;
+		}
+		public void setAddress(String address) {
+			this.address = address;
+		}
+		public void setCategories(Set<String> categories) {
+			this.categories = categories;
+		}
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public void setDistance(double distance) {
+			this.distance = distance;
+		}
+		
+		public Item build() {
+			return new Item(this);
+		}
+		
+	}
+
+	// 如果不想要itembuild 可以给每个field提供setter
+	// 如果有setter 则ecapsulation就不好了 任意人可以修改field
+	
+	public String getItemId() {
+		return itemId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public Set<String> getCategories() {
+		return categories;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		try {
+			// json format: key-value pair
+			obj.put("item_id", itemId);
+			obj.put("name", name);
+			obj.put("rating", rating);
+			obj.put("address", address);
+			obj.put("categories", new JSONArray(categories));
+			obj.put("image_url", imageUrl);
+			obj.put("url", url);
+			obj.put("distance", distance);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+
+}
