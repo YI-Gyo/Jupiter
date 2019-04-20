c0e93a1d17ada4b5f02b3f2212a58b6e3e531231
YIWEN GUO <33103135+YI-Gyo@users.noreply.github.com>
2019-04-19 17:09:49 -0700

    Update TicketMasterAPI.java

--- src/external/TicketMasterAPI.java
+++ src/external/TicketMasterAPI.java
@@ -21,7 +21,7 @@ import internal.Item.ItemBuilder;
 public class TicketMasterAPI {
 	private static final String URL = "https://app.ticketmaster.com/discovery/v2/events.json";
 	private static final String DEFAULT_KEYWORD = ""; // no restriction
-	private static final String API_KEY = "KeHKrEJ9sHL1UebSobXgaD7eGRnb35Fz";
+	private static final String API_KEY = "*";
 
 	public List<Item> search(double lat, double lon, String keyword) {
 		if (keyword == null) {
